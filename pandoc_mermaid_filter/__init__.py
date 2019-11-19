c24f57ccc6b61b85083b0f78cb3822ce25d32f33
Loc Nguyen <l@nocsi.com>
2019-11-19 05:14:56 -0800

    k

---
+++ pandoc_mermaid_filter/__init__.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import hashlib
+import subprocess as sp
+import panflute as pf
+from shutil import which
+
+MERMAID_BIN = os.path.expanduser(os.environ.get('MERMAID_BIN', 'mermaid'))
+PUPPETEER_CFG = os.environ.get('PUPPETEER_CFG', None)
+
+class MermaidInline(object):
+    """
+    Converts Link inmermaid class to Image
+    requires `mmdc` in PATH
+    option can be provided as attributes or can set default values in yaml metadata block
+
+    option          | metadata              | default
+    ----------------|-----------------------|----------
+    font-family     | svgbob.font-family    | "Arial"
+    font-size       | svgbob.font-size      | 14
+    scale           | svgbob.scale          | 1
+    stroke_width    | svgbob.stroke-width   | 2
+
+    """
+    def __init__(self):
+        self.dir_to = "svg"
+        assert which("mmdc"), "mmdc is not in path"
+
+    def action(self, elem, doc):
+        if isinstance(elem, pf.Link) and "mermaid" in elem.classes:
+            fn = elem.url
+            options = elem.attributes
+            caption = elem.content
+
+            meta_theme = doc.get_metadata("mermaid.theme", "default")
+            meta_background_color = doc.get_metadata("mermaid.background-color", "white")
+            meta_css = doc.get_metadata("mermaid.css", "")
+
+            theme = options.get("font-family", meta_theme)
+            background_color = options.get("background-color", meta_background_color)
+            css = options.get("css", meta_css)
+
+            mermaid_option = " ".join([
+                "--theme {}".format(font_family) if font_family is not None else "",
+                "--backgroundColor {}".format(font_size) if font_size is not None else "",
+                "--cssFile {}".format(css) if css is not None else ""
+            ])
+
+            if PUPPETEER_CFG is not None:
+                mermaid_option.join("--puppeteerConfigFile {}".format(PUPPETEER_CFG))
+
+            if not os.path.exists(self.dir_to):
+                os.mkdir(self.dir_to)
+
+            data = open(fn, "r", encoding="utf-8").read()
+            counter = hashlib.sha1(data.encode("utf-8")).hexdigest()[:8]
+            self.basename = "/".join([self.dir_to, str(counter)])
+
+            _format = "svg"
+
+            fn = os.path.abspath(fn)
+            linkto = os.path.abspath(".".join([self.basename, _format])).replace("\\", "/")
+
+            command = "mmdc -i {} {} -o {}".format(fn, mermaid_option, linkto)
+            pf.debug(command)
+            sp.Popen(command, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+
+            pf.debug("[inline] generate memeraid from {} to {}".format(fn, linkto))
+            elem.classes.remove("mermaid")
+            elem = pf.Image(*capion, classes=elem.classes, url=linkto,
+                            identifier=elem.identifier, title="fig:", attributes=elem.attributes)
+            return elem
+
+def main(doc=None):
+    mi = MermaidInline()
+    pf.run_filters([mi.action], doc=doc)
+    return doc
+
+if __name__ == "__main__":
+    main()
